4f91aebad7c94814b7aeafd18c584ef1152d8faa
Cedrick Argueta <cdrckrgt@stanford.edu>
2019-02-26 14:55:21 +0100

    starting to FEBOL in python. did basic search domain and some skeleton for drone

---
+++ src/Drone.py
@@ -0,0 +1,15 @@
+'''
+Drone.py
+
+Cedrick Argueta
+cdrckrgt@stanford.edu
+
+the drone and its physics, movement
+'''
+
+class Drone(object):
+    def __init__(self):
+        pass
+
+if __name__ == '__main__':
+    pass 
