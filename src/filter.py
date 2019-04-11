83304362d5bdee5c4e461078e93c37c8ec353c3b
Cedrick Argueta <cdrckrgt@stanford.edu>
2019-04-11 01:04:24 -0700

    adding more filter stuff

--- src/filter.py
+++ src/filter.py
@@ -10,4 +10,27 @@ import numpy as np
 
 class Filter(object):
     def __init__(self):
-        pass
+        raise Exception("Please instantitate a specific filter!")
+
+    def update(self):
+        raise Exception("Please instantitate a specific filter!")
+
+    def centroid(self):
+        raise Exception("Please instantitate a specific filter!")
+
+    def covariance(self):
+        raise Exception("Please instantitate a specific filter!")
+
+    def entropy(self):
+        raise Exception("Please instantitate a specific filter!")
+
+    def reset(self):
+        raise Exception("Please instantitate a specific filter!")
+
+
+class DiscreteFilter(Filter):
+    def __init__(self, domain, buckets, sensor):
+        self.domain = domain
+        self.df = np.ones(buckets, buckets) / (buckets ** 2) # buckets is num buckets per side
+        self.sensor = sensor
+        
