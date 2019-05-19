7d2295842692fdb378e6390eb250adf685a28d3e
Cedrick Argueta <cdrckrgt@stanford.edu>
2019-05-19 13:39:22 -0700

    adding entropy and distance cost model from louis\' paper. exciting...

--- PyFEBOL/cost.py
+++ PyFEBOL/cost.py
@@ -40,6 +40,50 @@ class SimpleDistanceCostModel(CostModel):
         x_theta, y_theta = domain.getTheta()
         return -np.linalg.norm(np.array([x, y]) - np.array([x_theta, y_theta]))
 
-class EntropyCost(CostModel):
+class EntropyCostModel(CostModel):
+    '''
+    returns the negative entropy of the filter.
+        - incentivizes ending the episode ASAP, lest it continue to accumulate large negative rewards
+        - also incentivizes keeping entropy low
+    '''
     def __init__(self):
         pass
+
+    def getCost(self, domain, drone, filter_, action):
+        entropy = filter_.entropy()
+        return -entropy
+
+class EntropyDistanceCostModel(CostModel):
+    '''
+    louis' cost model. cost is:
+        entropy + lambda * expectation over all bins (collision occurs)
+    
+    incentivizes keeping entropy low, while also staying a distance away from target
+    '''
+    def __init__(self, lambda_, threshold):
+        self.lambda_ = lambda_
+        self.threshold = threshold
+
+    def getCost(self, domain, drone, filter_, action):
+        entropy = filter_.entropy()
+
+        it = np.nditer(filter_.getBelief(), flags=['multi_index'])
+        expectation = 0
+        while not it.finished:
+            prob = it[0] # value of prob in this bucket
+            idx = it.multi_index # index of this bucket in filter (channel, x, y)
+            x = (idx[1] - 0.5) * filter_.cellSize
+            y = (idx[2] - 0.5) * filter_.cellSize
+            
+            x_seeker, y_seeker, _ = drone.getPose()
+            norm = np.linalg.norm(np.array([x, y]) - np.array([x_seeker, y_seeker]))
+
+            if norm < self.threshold: # if there's a collision for this bin
+                expectation += prob
+
+            it.iternext()
+
+        expectation *= self.lambda_
+
+        return entropy + expectation
+        
