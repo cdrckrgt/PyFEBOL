63eeb852156955ca25d70da9656eb94fb4f5648c
Cedrick Argueta <cedrick.argueta@aero.org>
2019-06-26 11:24:23 -0700

    changing init to make more sense'

--- PyFEBOL/searchdomain.py
+++ PyFEBOL/searchdomain.py
@@ -12,7 +12,7 @@ class SearchDomain(object):
     def __init__(self, length, policy=None, init=None):
         self.length = length
         if init: # fix position of target
-            self.theta = (init[0] * self.length, init[1] * self.length)
+            self.theta = init
         else:
             self.theta = (np.random.rand() * self.length, np.random.rand() * self.length)  # random RF source location
         self.policy = policy
