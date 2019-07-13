977eeb3ff47abd465847a958ec85ca0d0bfee1e9
Cedrick Argueta <cedrick.argueta@aero.org>
2019-07-12 22:04:52 -0700

    adding setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='PyFEBOL',
-      version='0.3.8.1',
+      version='0.3.9',
       description='Python Filter Exploration for Bearing Only Localization',
       url='https://github.com/cdrckrgt/PyFEBOL',
       author='Cedrick Argueta',
