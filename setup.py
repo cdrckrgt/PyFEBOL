1aa18a4a703a890f875930e4928c13ffb6fe500a
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-12-11 01:32:31 +0000

    adding setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='PyFEBOL',
-      version='0.4.7',
+      version='0.4.8',
       description='Python Filter Exploration for Bearing Only Localization',
       url='https://github.com/cdrckrgt/PyFEBOL',
       author='Cedrick Argueta',
